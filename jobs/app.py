939bd20577afba06be7e5bf69c571a45db6f6a8a
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-27 09:23:14 -0700

    initial commit

---
+++ jobs/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = 'db/jobs.sqlite'
+
+app = Flask(__name__)
+
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql():
+
+
+@app.route('/jobs')
+@app.route('/')
+def jobs():
+    return render_template('index.html')
